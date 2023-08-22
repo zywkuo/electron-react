318f2e941e9184eca8cf86d2c11b0bb520faf144
周艳伟 <zhouyanwei@sbr-info.com>
2023-08-22 09:36:34 +0800

    electron-react

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+// main初始化remote模块
+require('@electron/remote/main').initialize();
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 650,
+    minWidth: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // 判断开发和生产环境
+  const urlLocation = isDev ? 'http://localhost:8000' : '';
+  mainWindow.loadURL(urlLocation);
+});
